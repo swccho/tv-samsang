53e7133d66041612994e7f9473106c3bc1db7bf2
Khalid Imran <khalid@3dvi.com>
2024-05-24 07:21:15 +0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MyNewComponentComponent },
+  // other routes can be defined here
+];
